281b079e07006c5b4a959c298fcd8ce554471d02
Fermin <ferminresch13@gmail.com>
2026-06-08 00:32:19 -0300

    commit domingo 7/6

--- src/trabajopracticointegrador/ConexionDB/SocioDAO.java
+++ src/trabajopracticointegrador/ConexionDB/SocioDAO.java
@@ -4,10 +4,11 @@
  */
 package trabajopracticointegrador.ConexionDB;
 import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import trabajopracticointegrador.Socio;
+import java.time.LocalDate;
 
 /**
  *
@@ -19,9 +20,7 @@ public class SocioDAO {
     
     // ATRIBUTOS
     private Conexion conn;
-    private Connection connection;
     private ResultSet rs;
-    private Statement stm;
     private Socio socio;
     
     // CONSTRUCTOR
@@ -32,22 +31,34 @@ public class SocioDAO {
     // CONSULTAR SOCIO POR DNI
     public Socio obtenerSocio(String dni) throws SQLException {
         
-            conn = new Conexion("", "", "root", "");
-            connection = conn.getConexion();
-
-            stm = connection.createStatement();
-
-            rs = stm.executeQuery("SELECT * FROM Socios WHERE DNI = " + dni);
+            String consulta = "SELECT * FROM Personas p"
+                    + " INNER JOIN Socios s ON p.PersonaId = s.PersonaId"
+                    + " WHERE DNI = ?";
             
-            if (rs.next()) {
+            Socio socio = null;
+            
+            try (Connection connection = conn.getConexion()) {
                 
-                socio = new Socio();
-                socio.setNombre(rs.getString("nombre"));
+                PreparedStatement stm = connection.prepareStatement(consulta);
                 
-                // armar base de datos para que retorne directamente el socio
-                return socio;
+                stm.setString(1, dni);
+                
+                try (ResultSet rs = stm.executeQuery()) {
+                
+                    if (rs.next()) {
+                        socio = new Socio();
+                        socio.setNombre(rs.getString("Nombre"));
+                        socio.setApellido(rs.getString("Apellido"));
+                        socio.setDNI(rs.getString("DNI"));
+                        socio.setDireccion(rs.getString("Direccion"));
+                        socio.setNumeroTelefono(rs.getString("Telefono"));
+                        socio.setFechaNacimiento(rs.getObject("FechaNacimiento", LocalDate.class));
+                        socio.setFechaAlta(rs.getObject("FechaAlta", LocalDate.class));
+                        socio.setActivo(rs.getBoolean("Activo"));
+                    }
+                
+                }
             }
-            
-            return null;
+            return socio;
     }
 }
